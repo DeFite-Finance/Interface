27d1b85ada6a792ffc0a580e3f73b73f3dd1b9f9
AlbertLin0327 <linhsinkai@gmail.com>
2022-02-03 16:18:20 +0800

    2022/02/03 V2

--- static/js/14.ebfc9a89.chunk.js
+++ static/js/14.ebfc9a89.chunk.js
@@ -1,2 +1,1 @@
 (this["webpackJsonpiris-fintech-landing-page"]=this["webpackJsonpiris-fintech-landing-page"]||[]).push([[14],{902:function(e,n,i){"use strict";i.r(n);i(0);var t=i(1),c=i(6),s=function(){return Object(c.jsx)("div",{children:"Home"})};n.default=function(){return Object(c.jsx)(t.c,{children:Object(c.jsx)(t.a,{exact:!0,path:"/home",children:Object(c.jsx)(s,{})})})}}}]);
-//# sourceMappingURL=14.ebfc9a89.chunk.js.map
--- static/js/16.a29c6f9b.chunk.js
+++ static/js/16.a29c6f9b.chunk.js
@@ -1,2 +1,1 @@
 (this["webpackJsonpiris-fintech-landing-page"]=this["webpackJsonpiris-fintech-landing-page"]||[]).push([[16],{759:function(i,n){}}]);
-//# sourceMappingURL=16.a29c6f9b.chunk.js.map
